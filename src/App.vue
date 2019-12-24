27058b0b856b2e57f0623ec28ed4cf56a7ac0262
N V Hồng <Nguyen Van Hong>
2019-12-24 15:07:29 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <hello-world msg="Welcome to Your Vue.js App"/>
+    <test />
+  </div>
+</template>
+
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import test from './components/test'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    test
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
